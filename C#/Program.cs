eaf4f4dd0aa90de77ec76017f088dc12e4c057fd
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-06 01:51:02 +0700

    hoc
    commit lan 1

---
+++ C#/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+if{
+    Console.WriteLine("Hello, World!");
+}
+else
+{
+    Console.WriteLine("Hello, World!");
