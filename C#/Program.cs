35ed516800630cb0730cb943f7ccc8d7482eb016
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-07 20:28:07 +0700

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-if{
-    Console.WriteLine("Hello, World!");
-}
-else
-{
-    Console.WriteLine("Hello, World!");
