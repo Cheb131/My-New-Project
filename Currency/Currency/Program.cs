ef6bec738796f69d51a59586b87f1f4176ef39bf
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-11 21:17:25 +0700

    PJ

---
+++ Currency/Currency/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SystemTime;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int VND,USD;
+        Console.Write("Nhap so USD: ");
+        USD = int.Parse(Console.ReadLine());
+        VND = USD * 23000;
+        Console.WriteLine("So tien VND tuong ung la: " + VND);
+
+    }
+}
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Your name is: ");
-string name = Console.ReadLine();
-Console.WriteLine("Hello " + name);
+using System;
+
+namespace SystemTime;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Your name is: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello: " + name);
+
+    }
+}
