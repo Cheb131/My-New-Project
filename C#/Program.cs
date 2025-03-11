e1283f35ef7199fe864336a681642a5eca0d956b
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-11 20:33:11 +0700

    Time
    commit time

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+<<<<<<< Updated upstream
 if{
     Console.WriteLine("Hello, World!");
 }
 else
 {
     Console.WriteLine("Hello, World!");
+=======
+int a = 10;
+++a;
+Console.WriteLine("a =" + a);
+>>>>>>> Stashed changes
