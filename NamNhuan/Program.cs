13ff2ca2b5195fc7e4a06a95cc5fd1565a0ee70c
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-13 11:05:11 +0700

    LeapYear

---
+++ NamNhuan/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.Design;
+namespace NamNhuan
+{
+    class Program
+    {
+        static void Main()
+        {
+            int year;
+            bool isLeapYear = false;
+            Console.Write("Enter a year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            bool isDivisibleBy4 = year % 4 == 0;
+            if (isDivisibleBy4)
+            {
+                bool isDivisibleBy100 = year % 100 == 0;
+                if (isDivisibleBy100)
+                {
+                    bool isDivisibleBy400 = year % 400 == 0;
+                    if (isDivisibleBy400)
+                    {
+                        isLeapYear = true;
+                    }
+                }
+                else
+                {
+                    isLeapYear = true;
+                }
+            }
+            if (isLeapYear)
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT a leap year", year);
+            }
+        }
+    }
+}
