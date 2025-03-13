575d0b9ea63489f4675764e0447157ae555f9cc1
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-13 10:48:47 +0700

    BMI

---
+++ BMI/BMI/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your weight: " );
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your height: " );
+            double height = Convert.ToDouble(Console.ReadLine());
+            double bmi = weight / Math.Pow(height, 2);
+            Console.WriteLine("Your BMI is: " + bmi);
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if (bmi>=18.5 && bmi<25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi >= 25 && bmi < 30)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Obese");
+            }
+            {
+
+            }
+        }
+    }
+}
