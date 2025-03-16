6effbf78ff1886a47e0ad517656fc238b329be9e
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-16 20:25:30 +0700

    InteretRate

---
+++ InteretRate/InteretRate/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+    
+        Console.Write("So tien da vay: ");
+        double money = double.Parse(Console.ReadLine());
+        Console.Write("Lai suat: ");
+        double interestRate = double.Parse(Console.ReadLine());
+        Console.Write("So thang: ");
+        int month = int.Parse(Console.ReadLine());
+        double totalInterset = 0;
+        for (int i = 0; i < month; i++)
+        {
+            totalInterset += money * (interestRate / 100) / 12;
+            Console.WriteLine("So tien lai: " + totalInterset);
+        }
+   
+
+
