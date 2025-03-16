1b621e86fdaadda3c0e9aaf301d000062f21b3c5
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-16 21:35:44 +0700

    PrimeNumber

--- PrimeNumber/PrimeNumber/Program.cs
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
 int dem = 0;
 Console.WriteLine("Danh sach so nguyen to :");
@@ -7,14 +6,39 @@ for (int i = 2; i <= 100; i++)
 {
     for (int j = 1; j <= i; j++)
     {
-        if (i % j == 0)
+        static int isPrimeNumber(int n)
         {
-            dem++;
+            if (n < 2)
+            {
+                return 0;
+            }
+            int i;
+            int squareRoot = (int)Math.Sqrt(n);
+            for (i = 2; i <= squareRoot; i++)
+            {
+                if (n % i == 0)
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so nguyen n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0} so nguyen to dau tien la: \n", n);
+            int dem = 0;
+            int i = 2;   
+            while (dem < n)
+            {
+                if (isPrimeNumber(i) == 1)
+                {
+                    Console.Write("{0} ", i);
+                    dem++;
+                }
+                i++;
+            }
         }
     }
-
-    if (dem == 2)
-    {
-        Console.WriteLine(i);
-    }
-}
