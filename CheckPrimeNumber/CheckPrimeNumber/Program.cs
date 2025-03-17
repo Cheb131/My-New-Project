6df28218dc19d475f934dcd11ca204966169219a
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-17 11:40:36 +0700

    wd

---
+++ CheckPrimeNumber/CheckPrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap so nguyen: ");
+        int a = int.Parse(Console.ReadLine());
+        if (a < 2)
+        {
+            Console.WriteLine(a + " khong phai la so nguyen to");
+        }
+        else
+        {
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(a))
+            {
+                if (a % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+            {
+                Console.WriteLine(a + " la so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine(a + " khong phai so nguyen to");
+            }
+        }
+    }
+}
---
+++ UCLN/UCLN/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace UCLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            a = Math.Abs(a);
+            Console.Write("Nhap so nguyen b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            b = Math.Abs(b);
+            int c;
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("Khong co UCLN");
+            }
+            else
+            {
+                while (b != 0)
+                {
+                    c = a % b;
+                    a = b;
+                    b = c;
+                }
+                Console.WriteLine("UCLN la: " + a);
+            }
+        }
+        
+    }
+}
