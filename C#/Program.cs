952225e31dbdf7f9513ef6062ad859c2731127d2
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-19 15:56:36 +0700

    DelArr

---
+++ C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so phan tu cua mang: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.Write("Nhap so X can chen: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap vao vi tri can chen: ");
+            int vt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+               if (i < 0 && i > 9)
+                {
+                    Console.Write("Khong the chen vao mang");
+                }
+                else
+                {
+                    arr[vt] = x;
+                    Console.Write(arr[i] + " ");
+                }
+            }
+
+        }
+    }
+}
---
+++ DelArr/DelArr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace DelArr
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Nhap phan tu thu {0}: ", i + 1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Mang sau khi nhap: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write("Nhap phan tu can xoa: ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == x)
+                {
+                    arr[i] = arr[i + 1];
+                    Console.WriteLine("Mang sau khi xoa: ");
+                    for (int j = 0; j < arr.Length - 1; j++)
+                    {
+                        Console.Write(arr[j] + " ");
+                    }
+                }
+            }
+        }     
+    }
+}
