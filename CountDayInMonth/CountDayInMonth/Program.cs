ffb641f5b5bfacd2390c5adbddd5ac9825388667
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-24 21:47:14 +0700

    Class

--- CountDayInMonth/CountDayInMonth/Program.cs
+++ CountDayInMonth/CountDayInMonth/Program.cs
@@ -1,36 +1,141 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
- 
-    Console.Write("Which month that you want to count days? ");
-    int month = Int32.Parse(Console.ReadLine());
-string DayOfMonth = "";
-switch (month)
+namespace ListExample
 {
 
-    case 2:
-        Console.WriteLine("The month {0} has 28 or 29 days!", month);
-        DayOfMonth = "28 or 29";
-        break;
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        Console.WriteLine("The month {0} has 31 days!", month);
-        DayOfMonth = "31";
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        Console.WriteLine("The month {0} has 30 days!", month);
-        DayOfMonth = "30";
-        break;
-    default:
-        Console.WriteLine("Invalid input!");
-        break;     
+
+
+    public enum Color
+    {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        Yellow = 3,
+        White = 4,
+    }
+
+    public class House
+    {
+
+        public string name;
+        public int numberPerson;
+        public string addhouse;
+        public Color color;
+
+        public House(string namex, int numberPersonx, string addhousex, Color colorx)
+        {
+          
+            name = namex;
+            numberPerson = numberPersonx;
+            addhouse = addhousex;
+            color = colorx;
+        }
+
+    }
+    public class Test
+    {
+        static Dictionary<int, Dictionary<Color, List<House>>> dictFinal = new Dictionary<int, Dictionary<Color, List<House>>>();
+       
+
+
+        public static List<House> AddNewHouses()
+        {
+            List<House> test = new List<House>
+            {
+                new House("h1",3,"abc",Color.Red),
+                new House("h2",2,"abc",Color.Blue),
+                new House("h3",3,"abc",Color.Red),
+                new House("h4",4,"abc",Color.Blue),
+                new House("h5",5,"abc",Color.Red),
+                new House("h6",6,"abc",Color.Blue),
+                new House("h7",6,"abcD",Color.Red),
+                new House("h8",6,"abcDe",Color.Red),
+
+
+            };
+            return test;
+        }
+
+        public static void Main(string[] args)
+        {           
+
+            // Dictionary
+            Console.WriteLine("Dictionary");
+
+            List<House> listHouseMyDinh = AddNewHouses();       
+            foreach (var house in listHouseMyDinh)
+            {
+                
+                int number = house.numberPerson;
+
+
+                if (dictFinal.ContainsKey(number)) 
+                {
+                    AddHouseToDictByColor(house, dictFinal[number]);
+                }
+                else 
+                {
+                    Color color = house.color;
+                  
+                    Dictionary<Color, List<House>> dictChild = new Dictionary<Color, List<House>>();
+                    AddHouseToDictByColor(house, dictChild);
+                    dictFinal.Add(number, dictChild); 
+                }
+            }
+
+            FindListHouse(6, Color.White);
+
+
+        }
+
+
+        public static void FindListHouse(int number, Color color)
+        {
+            if (!dictFinal.ContainsKey(number))
+            {
+                Console.WriteLine("khong tim dc");
+                return;
+            }
+            var childDict = dictFinal[number];
+            if (!childDict.ContainsKey(color))
+            {
+                Console.WriteLine("khong tim dc");
+                return;
+            }
+            var listHouse = childDict[color];
+            foreach (var house in listHouse)
+            {
+                Console.WriteLine("house" + house.name);
+            }
+        }
+
+
+        public static void AddHouseToDictByColor(House house, Dictionary<Color, List<House>> childDict)
+        {
+            Color color = house.color;
+            if (childDict.ContainsKey(color))
+            {
+                childDict[color].Add(house);
+            }
+            else
+            {
+                childDict.Add(color, new List<House> { house });
+
+            }
+        }
+
 }
 
