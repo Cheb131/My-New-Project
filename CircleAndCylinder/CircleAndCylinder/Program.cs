f7d93876fbd2940e48320c715203bbd52b557925
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-03-29 14:23:02 +0700

    KeThua

---
+++ CircleAndCylinder/CircleAndCylinder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+public class Circle
+{
+    private double Radius { get; set; }
+    private string Color { get; set; }
+    public float Area;
+    public float Perimeter;
+    public Circle()
+    {        
+    
+    }
+    public Circle(double radius, string color)
+    {
+        this.Radius = radius;
+        this.Color = color;
+    }
+    public virtual float GetArea()
+    {
+       return Area = (float)(Math.PI * Math.Pow(Radius, 2));
+    }
+    public virtual float GetPerimeter()
+    {
+        return Perimeter = (float)(2 * Math.PI * Radius);
+    }
+    public class Cylinder : Circle
+    {
+        private double Height { get; set; }
+        public float Volume;
+        public Cylinder(double radius, string color, double height) : base(radius, color)
+        {
+            Height = height;
+        }
+        public override float GetArea()
+        {
+            base.GetArea();
+            Area = (float)(2 * Math.PI * Radius * (Radius + Height));
+            return Area;
+        }
+        public override float GetPerimeter()
+        {
+            base.GetPerimeter();
+            Perimeter = (float)(2 * Math.PI * Radius * (Radius + Height));
+            return Perimeter;
+        }
+        public void GetVolume()
+        {
+            Volume = (float)(Math.PI * Math.Pow(Radius, 2) * Height);
+        }
+    }
+    static void Main(string[] args)
+    {        
+        Circle circle1 = new Circle(5, "Red");
+        circle1.GetArea();
+        circle1.GetPerimeter();
+        Console.WriteLine("Circle1 Area: " + circle1.Area + "\nPerimeter: " +circle1.Perimeter+ "\ncolor: "+ circle1.Color);
+
+        Cylinder cylinder1 = new Cylinder(5, "Blue", 10);
+        cylinder1.GetArea();
+        cylinder1.GetPerimeter();
+        cylinder1.GetVolume();
+        Console.WriteLine("Cylinder1 Area: " + cylinder1.Area + "\nPerimeter: " + cylinder1.Perimeter + "\nVolume: " + cylinder1.Volume+ "\ncolor: "+cylinder1.Color);
+    }
+
+}
---
+++ Point2DAndPoint3D/Point2DAndPoint3D/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+public class Point2D
+{
+    public float x;
+    public float y;
+    public Point2D()
+    {
+    }
+    public Point2D(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void SetX(float x)
+    {
+        this.x = x;        
+    }
+    public float GetX()
+    {
+        return x;
+    }
+    public void SetY(float y)
+    {
+        this.y = y;
+    }
+    public float GetY()
+    {
+        return y;
+    }
+    public void SetXY(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public float[] GetXY()
+    {
+        float[] arr = { x, y };
+        return arr;
+    }
+    public override string ToString()
+    {
+        return "(" + x + "," + y + ")";
+    }
+    public class point3D : Point2D
+    {
+        public float z;
+        public point3D()
+        {
+        
+        }
+        public point3D(float x, float y, float z) : base(x, y)
+        {
+            this.z = z;
+        }
+        public void SetZ(float z)
+        {
+            this.z = z;
+        }
+        public float GetZ()
+        {
+            return z;
+        }
+       public void SetXYZ(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public float[] GetXYZ()
+        {
+            float[] arr = { x, y, z };
+            return arr;
+        }
+        public override string ToString()
+        {
+            return "(" + x + "," + y + "," + z + ")";
+        }
+    }
+    public static void Main()
+    {
+        Point2D p1 = new Point2D(1, 2);
+        Console.WriteLine(p1);
+        Point2D.point3D p2 = new Point2D.point3D(3, 4, 5);
+        Console.WriteLine(p2);
+    }
+}
+
